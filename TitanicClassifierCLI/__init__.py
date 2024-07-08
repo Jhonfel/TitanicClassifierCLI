8c87d2bd24c90287aea16eaa02c4ab04f6cfe4a1
Jhonfel <jhonfeldel@gmail.com>
2024-07-07 20:36:45 -0500

    added CLI

--- TitanicClassifierCLI/__init__.py
+++ TitanicClassifierCLI/__init__.py
@@ -1,6 +1,7 @@
-# TitanicClassifierCLI/__init__.py
 from .data_processor import DataProcessor
 from .model_trainer import ModelTrainer
 from .evaluator import Evaluator
+from .cli import cli
 
-__all__ = ['DataProcessor', 'ModelTrainer', 'Evaluator']
+__all__ = ['DataProcessor', 'ModelTrainer', 'Evaluator', 'cli']
+
--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ setup(
     version="0.1",
     packages=find_packages(include=['TitanicClassifierCLI', 'TitanicClassifierCLI.*']),
     install_requires=[
+        'Click',
         "pandas",
         "numpy",
         "scikit-learn",
         "matplotlib",
         "seaborn"
     ], 
+    entry_points='''
+        [console_scripts]
+        titanic-cli=TitanicClassifierCLI.cli:cli
+    ''',
 )
